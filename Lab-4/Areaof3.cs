7246c63c804975d10a9e624cd3e36895e178ef09
Harsh Zala <140790325+harshzala1@users.noreply.github.com>
2025-06-18 13:30:05 +0530

    Add files via upload

---
+++ Lab-4/Areaof3.cs
@@ -0,0 +1,33 @@
+﻿//Write a programs to Find Area of Square, Rectangle and Circle using
+//Method Overloading. 
+class Areaof3
+{
+    public virtual double Area(double r)
+    {
+        return 3.14 * r * r; 
+    }
+
+}
+
+class Areaof3_Square : Areaof3
+{
+    public override double Area(double side)
+    {
+        return side * side; 
+    }
+}
+
+class Areaof3_Rectangle : Areaof3
+{
+    public double Area(double length, double breadth)
+    {
+        return length * breadth; 
+    }
+}
+class Areaof3_Circle : Areaof3
+{
+    public override double Area(double radius)
+    {
+        return 3.14 * radius * radius; 
+    }
+}
---
+++ Lab-4/Program.cs
@@ -0,0 +1,90 @@
+﻿//public class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        Addition_overloading add = new Addition_overloading();
+//        int intresult = add.addd(5, 10);
+//        float floatresuklt = add.addd(10.2f, 15.15f);
+//        Console.WriteLine(floatresuklt);
+//        Console.WriteLine(intresult);
+//    }
+//}
+
+
+
+//public class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        tri_sqare ts = new tri_sqare();
+//        int squareArea = ts.Area(5);
+//        int rectangleArea = ts.Area(5, 10);
+//        Console.WriteLine("Area of Square: " + squareArea);
+//        Console.WriteLine("Area of Rectangle: " + rectangleArea);
+//    }
+//}
+
+
+//public class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        RBI rbi = new RBI();
+//        rbi.CalculateIterest(1000, 5, 2);
+//        HDFC hdfc = new HDFC();
+//        hdfc.CalculateIterest(1000, 5, 3);
+//        SBI sbi = new SBI();
+//        sbi.CalculateIterest(1000, 5, 4);
+//        ICICI icici = new ICICI();
+//        icici.CalculateIterest(1000, 5, 5);
+//    }
+//}
+
+
+//public class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        Hospital hospital = new Hospital();
+//        hospital.HospitalDetails();
+//        Apollo apollo = new Apollo();
+//        apollo.HospitalDetails();
+//        Wockhardt wockhardt = new Wockhardt();
+//        wockhardt.HospitalDetails();
+//        Gokul_Superspeciality gokul = new Gokul_Superspeciality();
+//        gokul.HospitalDetails();
+//    }
+//}
+
+
+//class program
+//{
+//    public static void Main(string[] args)
+//    {
+//        Areaof3 area = new Areaof3();
+//        double circleArea = area.Area(5); 
+//        Console.WriteLine("Area of Circle: " + circleArea);
+//        Areaof3_Square square = new Areaof3_Square();
+//        double squareArea = square.Area(4);
+//        Console.WriteLine("Area of Square: " + squareArea);
+//        Areaof3_Rectangle rectangle = new Areaof3_Rectangle();
+//        double rectangleArea = rectangle.Area(5, 10);
+//        Console.WriteLine("Area of Rectangle: " + rectangleArea);
+//        Areaof3_Circle circle = new Areaof3_Circle();
+//        double circleArea2 = circle.Area(7);
+//        Console.WriteLine("Area of Circle (using Areaof3_Circle): " + circleArea2);
+//    }
+//}
+
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        BankAcc account = new BankAcc("John Doe", 1000.00m);
+        account.Deposit(500.00m);
+        account.Withdraw(200.00m);
+        account.Withdraw(1500.00m); 
+      \
+    }
+}
